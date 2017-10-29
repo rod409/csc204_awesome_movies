5bc40058b283e7383d00515540a93b82c7d5fce6
rod409 <radoyeh@gmail.com>
2017-10-29 13:03:57 -0700

    Add queries to create database tables

---
+++ sql/create_tables.sql
@@ -0,0 +1,82 @@
+CREATE TABLE Movie (
+	id  INT NOT NULL,
+	title VARCHAR(64),
+	imdbPictureURL VARCHAR(128),
+	`year` YEAR,
+	rtAudienceRating Decimal(5, 1),
+	rtAudienceNumRatings INT,
+	rtAudienceScore SMALLINT,
+	rtPictureURL VARCHAR(128),
+	directorID VARCHAR(32) NOT NULL,
+	country VARCHAR(32),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Person (
+	id VARCHAR(32) NOT NULL,
+	`name` VARCHAR(64), 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Tag (
+	id INT,
+	value VARCHAR(64), 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Movie_Actor(
+	movieID INT NOT NULL,
+    actorID VARCHAR(32) NOT NULL,
+    ranking SMALLINT,
+    PRIMARY KEY(movieID, actorID)
+);
+
+CREATE TABLE Movie_Tag(
+	movieID INT NOT NULL,
+    tagID INT NOT NULL,
+    tagWeight INT,
+    PRIMARY KEY(movieID, tagID)
+);
+
+CREATE TABLE User_Rated_Movie (
+	userID INT NOT NULL,
+    movieID INT NOT NULL,
+    rating DECIMAL(5,1),
+    `timestamp` TIMESTAMP,
+    PRIMARY KEY(userID, movieID)
+);
+
+CREATE TABLE User_Tagged_Movie (
+	userID INT NOT NULL,
+    movieID INT NOT NULL,
+    tagID INT NOT NULL,
+    `timestamp` TIMESTAMP,
+    PRIMARY KEY(userID, movieID, tagID)
+);
+
+CREATE TABLE Movie_Genre(
+	movieID INT NOT NULL,
+    Genre VARCHAR(32),
+    PRIMARY KEY(movieID, Genre)
+);
+
+ALTER TABLE Movie
+	ADD FOREIGN KEY(directorID) REFERENCES Person(id);
+    
+ALTER TABLE Movie_Actor
+	ADD FOREIGN KEY(movieID) REFERENCES Movie(id),
+    ADD FOREIGN KEY(actorID) REFERENCES Person(id);
+    
+ALTER TABLE Movie_Tag
+	ADD FOREIGN KEY(movieID) REFERENCES Movie(id),
+    ADD FOREIGN KEY(tagID) REFERENCES Tag(id);
+    
+ALTER TABLE User_Rated_Movie
+	ADD FOREIGN KEY(movieID) REFERENCES Movie(id);
+
+ALTER TABLE User_Tagged_Movie
+	ADD FOREIGN KEY(movieID) REFERENCES Movie(id),
+    ADD FOREIGN KEY(tagID) REFERENCES Tag(id);
+
+ALTER TABLE Movie_Genre
+	ADD FOREIGN KEY(movieID) REFERENCES Movie(id);
