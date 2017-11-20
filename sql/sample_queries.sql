8d474717a052f1827db0a8d82dfedfc684dc3b6b
rod409 <radoyeh@gmail.com>
2017-11-19 23:12:36 -0800

    Add sample queries
    -For a query where a user specifies a parameter, a value is given in the sample

---
+++ sql/sample_queries.sql
@@ -0,0 +1,48 @@
+SELECT title, imdbPictureURL, `year`, rtAudienceScore, rtPictureURL
+FROM Movie
+ORDER BY rtAudienceScore DESC
+LIMIT 10;
+
+SELECT M.id, M.title, M.imdbPictureURL, M.year, M.rtAudienceScore, M.rtPictureURL, T.value
+FROM Movie as M, Tag as T, Movie_Tag as MT
+WHERE M.title LIKE '%Toy%' AND MT.movieID = M.id AND MT.tagID = T.id;
+
+SELECT M.id, M.title, M.imdbPictureURL, M.year, M.rtAudienceScore, M.rtPictureURL
+FROM Movie as M, Movie_Genre as MG
+WHERE MG.Genre = 'Comedy' AND MG.movieID = M.id
+ORDER BY rtAudienceScore DESC
+Limit 10;
+
+SELECT M.id, M.title, M.imdbPictureURL, M.year, M.rtAudienceScore, M.rtPictureURL
+FROM Movie as M, Person as P
+WHERE P.name LIKE "%Stone%" AND P.id = M.directorID;
+
+SELECT M.id, M.title, M.imdbPictureURL, M.year, M.rtAudienceScore, M.rtPictureURL
+FROM Movie as M, Person as P, Movie_Actor as MA
+WHERE P.name LIKE "%Smith%" AND P.id = MA.actorID AND M.id = MA.movieID;
+
+SELECT M.id, M.title, M.imdbPictureURL, M.year, M.rtAudienceScore, M.rtPictureURL, T.value
+FROM Movie as M, Tag as T, Movie_Tag as MT
+WHERE T.value LIKE '%robots%' AND MT.movieID = M.id AND MT.tagID = T.id
+ORDER BY rtAudienceScore DESC;
+
+SELECT P.name, AVG(M.rtAudienceScore) as Rating
+FROM Person as P, Movie as M
+WHERE M.directorID = P.id
+GROUP BY P.name
+Having COUNT(*) >= 5
+ORDER BY Rating DESC
+LIMIT 10;
+
+SELECT P.name, AVG(M.rtAudienceScore) as Rating
+FROM Person as P, Movie as M, Movie_Actor as MA
+WHERE M.id = MA.movieID AND P.id = MA.actorID
+GROUP BY P.name
+Having COUNT(*) >= 5
+ORDER BY Rating DESC
+LIMIT 10;
+
+SELECT M.title, MG.Genre
+FROM Movie as M, User_Rated_Movie as URM, Movie_Genre as MG
+WHERE URM.userID = 78 AND URM.movieID = M.id AND M.id = MG.movieID
+GROUP BY Genre;
