2bcb42138d82e661396cfdf5f2edbe647fa2d9f9
rod409 <radoyeh@gmail.com>
2017-11-19 22:46:53 -0800

    Add queries to drop tables

---
+++ sql/drop_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE Movie_Location;
+DROP TABLE Movie_Genre;
+DROP TABLE User_Tagged_Movie;
+DROP TABLE User_Rated_Movie;
+DROP TABLE Movie_Tag;
+DROP TABLE Movie_Actor;
+DROP TABLE Tag;
+DROP TABLE Movie;
+DROP TABLE Person;
