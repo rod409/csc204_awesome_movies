f4a7d281de2bfa80d91a645925ab3ad2e20421cc
rod409 <radoyeh@gmail.com>
2017-11-21 12:07:43 -0800

    Add a sample insert query for basic testing

---
+++ sql/sample_inserts.sql
@@ -0,0 +1,21 @@
+INSERT INTO Movie
+VALUES (
+	1,
+	'Toy Story',
+	'http://ia.media-imdb.com/images/M/MV5BMTMwNDU0NTY2Nl5BMl5BanBnXkFtZTcwOTUxOTM5Mw@@._V1._SX214_CR0,0,214,314_.jpg',
+	1995,
+    3.7,
+    102338,
+    81,
+    'http://content7.flixster.com/movie/10/93/63/10936393_det.jpg',
+    NULL, NULL),
+    (
+    2,
+	'Jumanji',
+	'http://ia.media-imdb.com/images/M/MV5BMzM5NjE1OTMxNV5BMl5BanBnXkFtZTcwNDY2MzEzMQ@@._V1._SY314_CR3,0,214,314_.jpg',
+	1995,
+    3.2,
+    44587,
+    61,
+    'http://content8.flixster.com/movie/56/79/73/5679734_det.jpg',
+    NULL, NULL);
